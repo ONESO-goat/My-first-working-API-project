08c8d83fd02ddae4dc9dfd23f7762b8c44a49e6a
Oneso <cylean34@gmail.com>
2025-12-29 19:12:21 -0500

    Initialize Spring Boot application structure

---
+++ backendJAVA/SpringbotappApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbotapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbotappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbotappApplication.class, args);
+	}
+
+}
